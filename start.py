e84a9bd9333ed26bb4c19d243bb8ce81bb557738
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-02-19 21:20:17 +0500

    начало бота

---
+++ start.py
@@ -0,0 +1,70 @@
+import telebot
+
+bot = telebot.TeleBot('6612359471:AAGs7LvXMqkan8OjI9H6YG1prp7QUbkH9pw')
+
+stats = {}
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Привет! Я - бот для определения токсичных людей в твоих чатах.')
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message, '/kick - кикнуть пользователя из чата\n/mute - замьютить пользователя,'
+                          'чтобы не токсичил\n/unmute - размьютить пользователя\n/stats - показать'
+                          'статистику токсичных пользователей\n/selfstat - показать свою статистику'
+                          'токсичности')
+
+@bot.message_handler(commands=['kick'])
+def kick_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно кикнуть администратора.")
+        else:
+            bot.kick_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} был кикнут.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите кикнуть.")
+
+def mute_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно замьютить администратора.")
+        else:
+            duration = 60
+            args = message.text.split()[1:]
+            if args:
+                try:
+                    duration = int(args[0])
+                except ValueError:
+                    bot.reply_to(message, "Неправильный формат времени.")
+                    return
+                if duration < 1:
+                    bot.reply_to(message, "Время должно быть положительным числом.")
+                    return
+                if duration > 1440:
+                    bot.reply_to(message, "Максимальное время - 1 день.")
+                    return
+            bot.restrict_chat_member(chat_id, user_id, until_date=time.time()+duration*60)
+            bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} замьючен на {duration} минут.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите замьютить.")
+
+@bot.message_handler(commands=['unmute'])
+def unmute_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        bot.restrict_chat_member(chat_id, user_id, can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True, can_add_web_page_previews=True)
+        bot.reply_to(message, f"Пользователь {message.reply_to_message.from_user.username} размьючен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите размьютить.")
+
+
+
