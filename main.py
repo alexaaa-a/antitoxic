50f908972f01b3b1fca51119bd0188cd815b96d5
UiminaM <142613899+UiminaM@users.noreply.github.com>
2024-05-06 14:46:03 +0300

    add

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def reset_and_recreate_table(chat_id):
 async def additional_training(conn):
     new_toxic_data = []
     toxic = ["-1"] * 100
-    async with conn.execute('SELECT message FROM toxic_message') as cursor:
+    async with conn.execute('SELECT word FROM words') as cursor:
         async for string in cursor:
             new_toxic_data.append(string[0])
     new_toxic_data.append("Привет, как дела?", "Доброе утро, как настроение?", "Пока, до встречи!",
@@ -249,12 +249,12 @@ async def add_toxic_words(chat_id, member_id, toxic_word):
         print(f"Error adding toxic word: {e}")
 
 
-async def add_toxic_word(msg: Message):
+async def add_toxic_word(msg: Message, word: str):
     chat_id = msg.chat.id
     table_name = f'chat_{abs(chat_id)}'
     member_id = msg.from_user.id
     username = msg.from_user.first_name
-    toxic_word = msg.text  # Получаем текст сообщения, к которому отвечают
+    toxic_word = word  # Получаем текст сообщения, к которому отвечают
     await add_toxic_words(chat_id, member_id, toxic_word)
     # Добавление +1 балла в столбец points для участника чата
     async with aiosqlite.connect('chat_members.db') as conn:
@@ -457,7 +457,10 @@ async def predict(msg: Message):
     text = msg.text
     prediction = model.predict([text])
     if prediction == -1:
-        await add_toxic_word(msg)
+        text = text.split()
+        for x in text:
+            if model.predict([x]) == -1:
+                await add_toxic_word(msg, x)
 
 
 if __name__ == "__main__":
